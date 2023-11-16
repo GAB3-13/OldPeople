fb62bf01b57a2d8b2e4e0e70c4ff79aeb69665a4
DogmaIAmGod <douglas.t.james.iii@gmail.com>
2023-11-16 07:57:25 -0500

    Adding PhP

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,24 +1,44 @@
 <!DOCTYPE html>
+
+<?php
+session_start();
+echo "Hello";
+?>
+
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <title>Login Page</title>
   <link rel="stylesheet" href="/css/login.css">
 </head>
+
 <body>
+
+  <?php
+  if (!isset($_POST['Login'])) {
+      $email = "";
+      $password = "";
+  } else {
+      $email = $_POST['email'];
+      $password = $_POST['password'];
+  }
+      ?>
+
   <div class="login-container">
     <form class="login-form">
       <h2>Login</h2>
       <div class="input-group">
         <label for="email">Email</label>
-        <input type="email" id="email" name="email" required>
+        <input type="email" id="email" name="email" value="<?php echo $email;?>" required>
       </div>
       <div class="input-group">
         <label for="password">Password</label>
-        <input type="password" id="password" name="password" required>
+        <input type="password" id="password" name="password" value="<?php echo $password;?>" required>
       </div>
-      <button type="submit">Login</button>
+      <input type="submit" name="Login" value="Login"/>
     </form>
   </div>
 </body>
+
 </html>
