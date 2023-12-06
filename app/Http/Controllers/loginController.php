03418be7b99df2eee3596f47032759dc6182f1b8
DogmaIAmGod <50518897+DogmaIAmGod@users.noreply.github.com>
2023-12-06 10:26:55 -0500

    session stuff?

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -3,21 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Individuals; 
-use App\Models\password; 
+use App\Models\Individuals;
+use App\Models\password;
+
+
+
 
 
 class LoginController extends Controller
 {
     public function login()
-    {    
+    {
+        if(session_status() !== 1) {
+            session_start();
+        }
+        $session = session('userID','');
         return view('login');
-    }  
+    }
 
     public function validateLogin(Request $request)
 {
     $data = $request->all();
-    
+
     $individual = Individuals::where('email', $data['email'])->first();
 
     if ($individual) {
@@ -27,7 +34,10 @@ class LoginController extends Controller
             $passwordMatch = $savedPassword->password === $data['password'];
 
             if ($passwordMatch) {
-                return 'good'; 
+                $type = Individuals::where('email', $data['email'])
+                ->get('roleID');
+                $request->session()->put(['userID' => $type]);
+                return 'correct';
             } else {
                 return 'Incorrect password';
             }
