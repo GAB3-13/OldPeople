db66a57570620088afe7a5ad7772cfdc512bdbd8
Evan <evanrhaldeman@gmail.com>
2023-12-10 15:53:02 -0500

    TestCheck

--- resources/views/doctorHome.blade.php
+++ resources/views/doctorHome.blade.php
@@ -133,5 +133,88 @@
             // Add logic to handle submitting Till Date
         }
     </script>
+    @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="box">
+            <label for="rosterDate">Date:</label>
+            <input type="text" id="rosterDate" readonly>
+        </div>
+@include('Roster.blade.php', ['roster' => $roster])
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Supervisor</th>
+                    <th>Doctor</th>
+                    <th>Caregiver1</th>
+                    <th>Caregiver2</th>
+                    <th>Caregiver3</th>
+                    <th>Caregiver4</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <label for="supervisorName">Name:</label>
+                        <input type="text" id="supervisorName" readonly value="John Doe">
+                    </td>
+                    <td>
+                        <label for="doctorName">Name:</label>
+                        <input type="text" id="doctorName" readonly value="Dr. Smith">
+                    </td>
+                    <td>
+                        <label for="caregiver1Name">Name:</label>
+                        <input type="text" id="caregiver1Name" readonly value="Caregiver 1">
+                    </td>
+                    <td>
+                        <label for="caregiver2Name">Name:</label>
+                        <input type="text" id="caregiver2Name" readonly value="Caregiver 2">
+                    </td>
+                    <td>
+                        <label for="caregiver3Name">Name:</label>
+                        <input type="text" id="caregiver3Name" readonly value="Caregiver 3">
+                    </td>
+                    <td>
+                        <label for="caregiver4Name">Name:</label>
+                        <input type="text" id="caregiver4Name" readonly value="Caregiver 4">
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <label for="doctorGroupName">Patient Group:</label>
+                        <input type="text" id="doctorGroupName" readonly value="Group A">
+                    </td>
+                    <td>
+                        <label for="caregiver1GroupName">Patient Group:</label>
+                        <input type="text" id="caregiver1GroupName" readonly value="Group B">
+                    </td>
+                    <td>
+                        <label for="caregiver2GroupName">Patient Group:</label>
+                        <input type="text" id="caregiver2GroupName" readonly value="Group C">
+                    </td>
+                    <td>
+                        <label for="caregiver3GroupName">Patient Group:</label>
+                        <input type="text" id="caregiver3GroupName" readonly value="Group D">
+                    </td>
+                    <td>
+                        <label for="caregiver4GroupName">Patient Group:</label>
+                        <input type="text" id="caregiver4GroupName" readonly value="Group E">
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+@endsection
 </body>
 </html>
